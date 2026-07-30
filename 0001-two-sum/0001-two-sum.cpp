94d5931b609c4781683af28fa85f9dadadd0cc05
Gaurav Mishra <114816474+mishragaurav01@users.noreply.github.com>
2026-07-30 16:16:40 +0530

    Time: 4 ms (51.49%), Space: 15 MB (12.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,37 @@
+// Optimal Time O(n)
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i < nums.size() - 1; i++){
-            for(int j = i + 1; j < nums.size(); j++) {
-                if(nums[i] + nums[j] == target){
-                    return {i,j};
-                }
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            if(mp.count(complement)){
+                return {mp[complement], i};
             }
+            mp[nums[i]] = i;
         }
+
         return {};
+
     }
 };
+
+
+
+// Brute force Time O(n^2)
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         for(int i = 0; i < nums.size() - 1; i++){
+//             for(int j = i + 1; j < nums.size(); j++) {
+//                 if(nums[i] + nums[j] == target){
+//                     return {i,j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
